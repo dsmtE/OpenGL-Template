4feadec7a22fed2b5e133a5fb36ae2283a0b2462
dsmtE <enguerrand.desmet@gmail.com>
2022-04-24 15:10:39 +0200

    initial setup

---
+++ src/App.cpp
@@ -0,0 +1,41 @@
+#include "App.hpp"
+
+#include "GLFW/glfw3.h"
+
+App::App() : _previousTime(0.0) {
+}
+
+void App::Update() {
+
+    double currentTime = glfwGetTime();
+    double elapsedTime = currentTime - _previousTime;
+    _previousTime = currentTime;
+    
+
+    Render();
+}
+
+void App::Render() {
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+}
+
+void App::key_callback(int /*key*/, int /*scancode*/, int /*action*/, int /*mods*/) {
+}
+
+void App::mouse_button_callback(int /*button*/, int /*action*/, int /*mods*/) {
+}
+
+void App::scroll_callback(double /*xoffset*/, double /*yoffset*/) {
+}
+
+void App::cursor_position_callback(double /*xpos*/, double /*ypos*/) {
+}
+
+void App::size_callback(int width, int height) {
+    _width  = width;
+    _height = height;
+
+    // make sure the viewport matches the new window dimensions
+    glViewport(0, 0, _width, _height);
+}
