d3a19dc424e16a96ffa396fe9329b13d0844dfd4
enguerrand DE SMET <enguerrand.de-smet@ubisoft.com>
2023-02-17 17:38:25 +0100

    Simplification without App class and TP1 setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
 #include "glad/glad.h"
+#include <iostream>
 
-#include "App.hpp"
+/* Minimal time wanted between two images */
+static const double FRAMERATE_IN_SECONDS = 1. / 30.;
 
-static App& get_app(GLFWwindow* window) {
-    return *reinterpret_cast<App*>(glfwGetWindowUserPointer(window));
+/* Error handling function */
+void onError(int error, const char* description) {
+    std::cout << "GLFW Error: " << description << std::endl;
 }
 
 int main() {
@@ -13,6 +16,9 @@ int main() {
     if (!glfwInit()) {
         return -1;
     }
+
+    /* Callback to a function if an error is rised by GLFW */
+	glfwSetErrorCallback(onError);
 
     // Create a windowed mode window and its OpenGL context
 #ifdef __APPLE__
@@ -36,45 +42,42 @@ int main() {
         return -1;
     }
 
-    // Create the App
-    App app;
-
-    // Hook user inputs to the App
-    glfwSetWindowUserPointer(window, reinterpret_cast<void*>(&app));
-    
     glfwSetKeyCallback(window, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
-        get_app(window).key_callback(key, scancode, action, mods);
-    });
-    glfwSetMouseButtonCallback(window, [](GLFWwindow* window, int button, int action, int mods) {
-        get_app(window).mouse_button_callback(button, action, mods);
-    });
-    glfwSetScrollCallback(window, [](GLFWwindow* window, double xoffset, double yoffset) {
-        get_app(window).scroll_callback(xoffset, yoffset);
-    });
-    glfwSetCursorPosCallback(window, [](GLFWwindow* window, double xpos, double ypos) {
-        get_app(window).cursor_position_callback(xpos, ypos);
-    });
-    glfwSetWindowSizeCallback(window, [](GLFWwindow* window, int width, int height) {
-        get_app(window).size_callback(width, height);
+        std::cout << "key pressed: " << key << ", " << scancode << std::endl;
     });
 
-    // Force call the size_callback of the app to set the right viewport and projection matrix
-    {
-        int width, height;
-        glfwGetWindowSize(window, &width, &height);
-        app.size_callback(width, height);
-    }
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Get time (in second) at loop beginning */
+		double startTime = glfwGetTime();
 
-    // Loop until the user closes the window
-    while (!glfwWindowShouldClose(window)) {
-        app.Update();
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        // Swap front and back buffers
-        glfwSwapBuffers(window);
+        // render exemple quad
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glBegin(GL_QUADS);
+            glVertex2f(-0.5f, -0.5f);
+            glVertex2f(0.5f, -0.5f);
+            glVertex2f(0.5f, 0.5f);
+            glVertex2f(-0.5f, 0.5f);
+        glEnd();
 
-        // Poll for and process events
-        glfwPollEvents();
-    }
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+
+		/* Elapsed time computation from loop begining */
+		double elapsedTime = glfwGetTime() - startTime;
+		/* If to few time is spend vs our wanted FPS, we wait */
+		if(elapsedTime < FRAMERATE_IN_SECONDS) 
+		{
+			glfwWaitEventsTimeout(FRAMERATE_IN_SECONDS-elapsedTime);
+		}
+	}
 
     glfwTerminate();
     return 0;
