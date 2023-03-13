e52acd634f0ad337b041a13a6b5d92d4d157a0b5
dsmtE <desmet.enguerrand@gmail.com>
2023-03-13 22:51:29 +0100

    Fix include and compilation errors in 3D_tools.h
    Add windows macro

--- src/3D_tools.h
+++ src/3D_tools.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+
+// Needed before including GL/glu.h 
+// to avoid a compilation error because of CALLBACK pre-processor definition.
+// See https://stackoverflow.com/questions/19198244/how-do-i-get-rid-of-these-compiler-errors-in-glu-h#comment28407913_19198244
+// It's usually defined in a system header to define the default calling convention for callbacks, 
+// but if it is defined incorrectly this sort of thing happens.
+// windef.h usually defines it is __stdcall
+#if defined(_WIN64) || defined(_WIN32)
+    #include <windef.h>
+#endif
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include <stdlib.h>
-#include <stdio.h>
+
+// important to add this line to use M_PI
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 #define NB_SEG_CIRCLE 64
